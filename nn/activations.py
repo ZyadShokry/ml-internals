60f1554dc0d1e7e45dec26f77a37c71ab4c30e36
Zyad Shokry <zyad.shokry@ejust.edu.eg>
2025-01-24 16:50:06 +0200

    added sigmoid, relu activations

---
+++ nn/activations.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+class Sigmoid:
+    def __init__(self):
+        # y: output
+        self.y = None
+
+    def forward(self, x):
+        self.y = 1.0 / (1.0 + np.exp(-x))
+        return self.y
+
+    def backward(self, dy):
+        # dy: gradient from the upper layer
+        return dy * self.y * (1.0 - self.y)
+
+    def __str__(self):
+        return 'Sigmoid'
+
+
+class ReLU:
+    def __init__(self):
+        # x: input
+        self.x = None
+
+    def forward(self, x):
+        self.x = x
+        return np.maximum(0.0, x)
+
+    def backward(self, dy):
+        # dy: gradient from the upper layer
+        return dy * (self.x > 0.0)
+
+    def __str__(self):
+        return 'ReLU'
